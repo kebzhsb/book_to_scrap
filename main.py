7359aa72a44707e3f65ecedbe4b3f6890a520d6a
kebzhsb <bremontquentin@gmail.com>
2025-11-04 17:39:44 +0100

    correction des a + modif indentation info

--- main.py
+++ main.py
@@ -4,31 +4,36 @@ from bs4 import BeautifulSoup
 
 url = "https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
 
-reponse = requests.get(url)
+response = requests.get(url)
 
-if reponse.ok :
-    soup = BeautifulSoup(reponse.text, "html.parser")
+if response.ok :
+    response.encoding = "utf-8"
+    soup = BeautifulSoup(response.text, "html.parser")
     
+    #Autres infos
     title = soup.find("h1").get_text(strip=True)
     description = soup.select_one("#product_description + p").get_text(strip=True)
     category = soup.select_one("ul.breadcrumb li:nth-of-type(3) a").get_text(strip=True)
     
-    
+
+    #Infos principales du tableau + créa du dico
     infos = {}
     for tr in soup.find_all("tr"):
         th = tr.find("th").get_text(strip=True)
         td = tr.find("td").get_text(strip=True)
         infos[th] = td
-       
-        infos["Title"] = title
-        infos["Category"] = category
-        infos["Description"] = description
-
+    # Ajout du reste au dico
+    infos["Title"] = title
+    infos["Category"] = category
+    infos["Description"] = description
+    # Crea Csv
     with open("book.csv", "w", newline="", encoding="utf-8-sig") as f:
         writer = csv.DictWriter(f, fieldnames=infos.keys(), delimiter=';')
         writer.writeheader()
         writer.writerow(infos)
+    
 
+# Régler probleme des Â , sinon début ok manque url de la page et de l'image 
 
 
 
